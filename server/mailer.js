a3dbab6789fc4352526a22228ed1bd0ff47261f5
thaismozelli-spec <thaismozelli@escritorial.com.br>
2026-07-04 22:09:39 -0300

    Update mailer.js

--- server/mailer.js
+++ server/mailer.js
@@ -18,11 +18,14 @@ function getTransporter() {
     return null;
   }
   _transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,          // 587 usa STARTTLS (não SSL direto)
+    requireTLS: true,
     auth: { user: GMAIL_USER, pass: GMAIL_APP_PASS },
-    connectionTimeout: 8000,  // 8s para conectar
-    greetingTimeout: 8000,
-    socketTimeout: 10000,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 12000,
   });
   return _transporter;
 }
